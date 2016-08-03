f9a72961fe317ac314f961bfb8a2071c36432668
paul <p.p.vanswinderen@student.utwente.nl>
2016-08-03 15:58:41 +0200

    Added math files

---
+++ math.cpp
@@ -0,0 +1,5 @@
+//
+// Created by paul on 8/3/16.
+//
+
+#include "math.h"
---
+++ math.h
@@ -0,0 +1,10 @@
+//
+// Created by paul on 8/3/16.
+//
+
+#ifndef PIE_GITHUB_MATH_H
+#define PIE_GITHUB_MATH_H
+
+
+
+#endif //PIE_GITHUB_MATH_H
