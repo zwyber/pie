b644f673581beed98b7f64c25fd7ba2e721c63af
YvanKlaver <yvan_klaver@hotmail.com>
2016-08-09 14:00:13 +0200

    Visualisation of universe works. Will comment code when it gets merged to test.cpp.

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -13,23 +13,49 @@ GLFWwindow* window;
 #include "../framework.h"
 
 //GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
-
-void drawFilledCircle(vec2d &pos, GLfloat &r, int num_segments, GLfloat &screenWtHRatio, std::array<double,4> Color)
+void DrawGrid(int stepSize)
 {
-    GLfloat theta = 2.0f / double(num_segments) * PI ;
-    GLfloat c = cosf(theta);//precalculate the sine and cosine
-    GLfloat s = sinf(theta);
-    GLfloat t;
+    //give stepSize in px
+    GLFWwindow* CurrentWindow = glfwGetCurrentContext();
+    int Width;
+    int Height;
+    glfwGetWindowSize(CurrentWindow, &Width, &Height);
+    glBegin(GL_LINES);
+    glColor3f(0.75f, 0.75f, 0.75f);
+    for(int i=0;i<=Width/2;i+=stepSize)
+    {
+        glVertex2d((double)i*2.0/Width,-1.0);
+        glVertex2d((double)i*2.0/Width,1.0);
+        glVertex2d((double)-i*2.0/Width,-1.0);
+        glVertex2d((double)-i*2.0/Width,1.0);
+    }
+    for(int i=0;i<=Height/2;i+=stepSize)
+    {
+        glVertex2d(-1.0,(double)i*2.0/Height);
+        glVertex2d(1.0,(double)i*2.0/Height);
+        glVertex2d(-1.0,(double)-i*2.0/Height);
+        glVertex2d(1.0,(double)-i*2.0/Height);
+    }
+    glEnd();
 
-    GLfloat x = r;//we start at angle = 0
-    GLfloat y = 0;
+}
+void drawFilledCircle(vec2d &pos, GLdouble &r, int num_segments, GLdouble &screenWtHRatio, std::array<double,4> Color)
+{
+    GLdouble phi = 2.0f / double(num_segments) * PI ;
+    GLdouble c = cosf(phi);//precalculate the sine and cosine
+    GLdouble s = sinf(phi);
+    GLdouble t;
 
+    GLdouble x = r;//we start at angle = 0
+    GLdouble y = 0;
+
+    //Start a surface (triangle fan)
     glBegin(GL_TRIANGLE_FAN);
     glColor4d(Color[0], Color[1], Color[2], Color[3]);
-    glVertex2f(x/screenWtHRatio +pos[0], y +pos[1]); // center of circle
+    glVertex2d(x/screenWtHRatio +pos[0], y +pos[1]); // center of circle
     for(int ii = 0; ii < num_segments; ii++)
     {
-        glVertex2f(x/screenWtHRatio + pos[0], y + pos[1]);//output vertex
+        glVertex2d(x/screenWtHRatio + pos[0], y + pos[1]);//output vertex
 
         //apply the rotation matrix
         t = x;
@@ -39,42 +65,49 @@ void drawFilledCircle(vec2d &pos, GLfloat &r, int num_segments, GLfloat &screenW
     glEnd();
 }
 
-void drawObjectList(std::vector<Object> &objects){
+void drawObjectList(std::vector<Object> &objects, double Uwidth, double Uheight){
     GLFWwindow* CurrentWindow = glfwGetCurrentContext();
     int Width;
     int Height;
     glfwGetWindowSize(CurrentWindow, &Width, &Height);
-    GLfloat WtHratio = (GLfloat)Width/Height;
+    GLdouble WtHratio = (GLdouble)Width/Height;
     for(int ii = 0; ii < objects.size(); ii++){
-        GLfloat radius = objects[ii].get_radius()/Height;
+        GLdouble radius = objects[ii].get_radius()/Uheight;
         vec2d position = objects[ii].get_position();
-        position[0] /= Width;
-        position[1] /= Height;
+        position[0] *= 1.0/Uwidth;
+        position[1] *= 1.0/Uheight;
         drawFilledCircle(position, radius, 100, WtHratio, objects[ii].get_colour());
+        //std::cout << "Position of object "<< ii << ": " << position[0] << "," << position[1] << "  \t";
     }
+    //std::cout << std::endl;
 }
 
 int main( void )
 {
-    int width = 1024;
-    int height = 768;
+    int width = 720;
+    int height = 480;
 
     Object A;
     Object B;
-    // Set positions and velocities
-    A.set_position(50, 20);
-    B.set_position(-1000, -700);
 
-    A.set_velocity(-2,-1);
-    B.set_velocity(1,1);
+    // Set them apart, and on a collision course
+    A.set_position(2, 0);
+    A.set_velocity(0, 0);
 
-    A.set_radius(50);
-    B.set_radius(20);
+
+    B.set_position(-2, 0);
+    B.set_velocity(0, 0);
+
     // Generate a universe
-    Universe universe(width, height);
+    Universe universe(width/50.0, height/50.0);
 
-    universe.add_object(A);
-    universe.add_object(B);
+    // Add them to the universe
+    id_type A_id = universe.add_object(A);
+    id_type B_id = universe.add_object(B);
+
+    Object* Ap = universe.get_object_by_id(A_id);
+    Object* Bp = universe.get_object_by_id(B_id);
+
     // Initialise GLFW
 	if( !glfwInit() )
 	{
@@ -112,10 +145,18 @@ int main( void )
 	// Dark blue background
 	glClearColor(0.5f, 0.5f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    unsigned IT = 0;
+    vec2d posA;
+    vec2d posB;
 	do{
 
-        drawObjectList(universe.objects);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        DrawGrid(50);
+        drawObjectList(universe.objects,universe.width, universe.height);
+        universe.physics_runtime_iteration();
+        posA = Ap->get_position();
+        posB = Bp->get_position();
+        //std::cout << "Position of A: " << posA[0] << "," << posA[1] << " Position of B: " << posB[0] << "," << posB[1] << ";" << std::endl;
 
         // Swap buffers
 		glfwSwapBuffers(window);
