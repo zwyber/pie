14471099f3f9ed38ef2f612b23bac47490f2cdb0
paul <p.p.vanswinderen@student.utwente.nl>
2016-08-10 17:17:06 +0200

    Moved to using pointers in the universe list

--- objects.h
+++ objects.h
@@ -30,15 +30,9 @@ private:
     double radius = 1;
     // Colour vector of this object {Red, Green, Blue, alpha}. All values are between 1 and 0
     std::array<double, 4> colour = {{(double)std::rand()/RAND_MAX,(double)std::rand()/RAND_MAX,(double)std::rand()/RAND_MAX,1.0}};
-    // Identification number definitions
-    id_type id = 0;
-    bool id_set = false; // Toggle for allowing a new id to be generated or not.
+
 
 public:
-    // Return the id of the object, return 0 if undefined
-    id_type get_id();
-    // Set to the id given. Advised to use Universe::give_new_object_id()
-    void set_id(id_type &new_id);
 
     // Bouncyness, the coefficient of restitution for this object
     double bouncyness = 1;
@@ -66,7 +60,7 @@ public:
     std::array<double, 4> get_colour();
 
     // Calculate new position and velocity function
-    std::array<vec2d, 2> calc_new_pos_vel (std::vector<Object> &objects, double &time_step, Physics &physics);
+    std::array<vec2d, 2> calc_new_pos_vel (std::vector<Object*> &objects, double &time_step, Physics &physics);
 
 };
 
@@ -94,7 +88,6 @@ class Universe {
 
 private:
     // To keep track of what the next available object_id is
-    id_type new_object_id = 0;
     double _Height;
     double _Width;
 
@@ -109,30 +102,17 @@ public:
     const double &width;
 
     // A vector of objects. This contains the objects in the world!
-    std::vector<Object> objects = {};
+    std::vector<Object*> objects = {};
 
     // Add a new object, return the new id of the object
-    id_type add_object (Object &obj);
+    void add_object (Object* &obj);
 
     // Remove objects, either by id or by index
     void remove_object_by_index(int obj_index);
-    bool remove_object_by_id(id_type id);
 
     // Get objects, either by id or by index
     Object* get_object_by_index(int index);
-    Object* get_object_by_id(id_type id);
-    std::vector<Object> get_all_objects ();
-
-    // Give out a new unique id for objects
-    id_type give_new_object_id();
-
-    // Function to update the mapping of id to object.
-    // MUST BE CALLED EVERY TIME THE UNIVERSE OBJECTS LIST CHANGES!
-    void update_id_to_object_map();
-
-    // Map for getting an object given the id
-    std::map<id_type, Object*> id_to_object_map;
-
+    std::vector<Object*> get_all_objects ();
 
     // Physics engine
     void physics_runtime_iteration ();
