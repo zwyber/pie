86ea3253292e64330e6f060d42df1538da409b03
zwyber <paul@vanswinderen.com>
2016-10-16 15:59:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,12 @@
 // Created by paul on 7/15/16.
 //
 
-#define PIE_ONLY_BACKEND
-
+// #define PIE_ONLY_BACKEND
 #include "framework.h"
+#include "lib/game.h"
 
 int main() {
-    //maingame();
-
-    // Create a universe
-    Universe universe;
-
-    // Add an object
-    Object* A = universe.add_object();
-
-    // Set parameters
-    A->set_position(3, 0);
-    A->set_velocity(-2, 1);
-
-    A->set_radius(2);
-    A->set_mass(2);
-    A->set_colour({1, 0, 0, 1}); // Red
-
-    // Add another object
-    Object* B = universe.add_object();
-    B->set_position(-3, 0);
-    B->set_velocity(2, -1);
-    B->set_mass(1);
-    B->set_radius(1);
-    B->set_colour({0, 0, 1, 1}); // Blue
-
+    maingame();
+    
     return 0;
 }
